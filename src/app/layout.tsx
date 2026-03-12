51ce4b19795da4ab8d4c370bd90a1e76565d0479
Helton Batista <heltonbts@icloud.com>
2026-03-12 12:29:23 -0300

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NutriTrack - Nutrição Comportamental",
+  title: "NutriTrack 2 - Nutrição Comportamental",
   description: "Acompanhamento nutricional comportamental",
 };
 
